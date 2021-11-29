b3cea54b55dde45276a41b30d431e3d474d3e62a
Nicolas Powers <powersn2@mymacewan.ca>
2021-11-29 03:10:08 -0700

    Current project files

---
+++ LilLegendViewer/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace LilLegendViewer
+{
+    partial class YIM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YIM));
+            this.cB_Character = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_Character = new System.Windows.Forms.Label();
+            this.label_Skin = new System.Windows.Forms.Label();
+            this.rb_LL = new System.Windows.Forms.RadioButton();
+            this.Label_HTTPFail = new System.Windows.Forms.Label();
+            this.Button_Load = new System.Windows.Forms.Button();
+            this.cB_Skin = new System.Windows.Forms.CheckedListBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lB_Images = new System.Windows.Forms.ListBox();
+            this.label_Images = new System.Windows.Forms.Label();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Display = new System.Windows.Forms.Button();
+            this.button_Remove = new System.Windows.Forms.Button();
+            this.pB_Image = new System.Windows.Forms.PictureBox();
+            this.label_NoSelectImage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pB_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cB_Character
+            // 
+            this.cB_Character.FormattingEnabled = true;
+            this.cB_Character.Location = new System.Drawing.Point(12, 103);
+            this.cB_Character.Name = "cB_Character";
+            this.cB_Character.Size = new System.Drawing.Size(182, 21);
+            this.cB_Character.TabIndex = 1;
+            this.cB_Character.Visible = false;
+            this.cB_Character.SelectedIndexChanged += new System.EventHandler(this.cB_Character_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Category";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_Character
+            // 
+            this.label_Character.AutoSize = true;
+            this.label_Character.Location = new System.Drawing.Point(9, 87);
+            this.label_Character.Name = "label_Character";
+            this.label_Character.Size = new System.Drawing.Size(53, 13);
+            this.label_Character.TabIndex = 4;
+            this.label_Character.Text = "Character";
+            this.label_Character.Visible = false;
+            // 
+            // label_Skin
+            // 
+            this.label_Skin.AutoSize = true;
+            this.label_Skin.Location = new System.Drawing.Point(9, 140);
+            this.label_Skin.Name = "label_Skin";
+            this.label_Skin.Size = new System.Drawing.Size(28, 13);
+            this.label_Skin.TabIndex = 5;
+            this.label_Skin.Text = "Skin";
+            this.label_Skin.Visible = false;
+            this.label_Skin.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // rb_LL
+            // 
+            this.rb_LL.AutoSize = true;
+            this.rb_LL.Checked = true;
+            this.rb_LL.Location = new System.Drawing.Point(12, 29);
+            this.rb_LL.Name = "rb_LL";
+            this.rb_LL.Size = new System.Drawing.Size(91, 17);
+            this.rb_LL.TabIndex = 6;
+            this.rb_LL.TabStop = true;
+            this.rb_LL.Text = "Little Legends";
+            this.rb_LL.UseVisualStyleBackColor = true;
+            this.rb_LL.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Label_HTTPFail
+            // 
+            this.Label_HTTPFail.AutoSize = true;
+            this.Label_HTTPFail.ForeColor = System.Drawing.Color.Red;
+            this.Label_HTTPFail.Location = new System.Drawing.Point(333, 133);
+            this.Label_HTTPFail.Name = "Label_HTTPFail";
+            this.Label_HTTPFail.Size = new System.Drawing.Size(196, 13);
+            this.Label_HTTPFail.TabIndex = 7;
+            this.Label_HTTPFail.Text = "HTTP Request Failed. Please Try Again";
+            this.Label_HTTPFail.Visible = false;
+            // 
+            // Button_Load
+            // 
+            this.Button_Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Load.Location = new System.Drawing.Point(12, 52);
+            this.Button_Load.Name = "Button_Load";
+            this.Button_Load.Size = new System.Drawing.Size(91, 23);
+            this.Button_Load.TabIndex = 8;
+            this.Button_Load.Text = "Load";
+            this.Button_Load.UseVisualStyleBackColor = true;
+            this.Button_Load.Click += new System.EventHandler(this.Button_Load_Click);
+            // 
+            // cB_Skin
+            // 
+            this.cB_Skin.CheckOnClick = true;
+            this.cB_Skin.FormattingEnabled = true;
+            this.cB_Skin.Location = new System.Drawing.Point(12, 158);
+            this.cB_Skin.Name = "cB_Skin";
+            this.cB_Skin.Size = new System.Drawing.Size(182, 724);
+            this.cB_Skin.Sorted = true;
+            this.cB_Skin.TabIndex = 9;
+            this.cB_Skin.Visible = false;
+            this.cB_Skin.SelectedIndexChanged += new System.EventHandler(this.cB_Skin_SelectedIndexChanged_1);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // lB_Images
+            // 
+            this.lB_Images.FormattingEnabled = true;
+            this.lB_Images.Location = new System.Drawing.Point(214, 29);
+            this.lB_Images.MultiColumn = true;
+            this.lB_Images.Name = "lB_Images";
+            this.lB_Images.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lB_Images.Size = new System.Drawing.Size(920, 95);
+            this.lB_Images.Sorted = true;
+            this.lB_Images.TabIndex = 10;
+            // 
+            // label_Images
+            // 
+            this.label_Images.AutoSize = true;
+            this.label_Images.Location = new System.Drawing.Point(211, 9);
+            this.label_Images.Name = "label_Images";
+            this.label_Images.Size = new System.Drawing.Size(80, 13);
+            this.label_Images.TabIndex = 11;
+            this.label_Images.Text = "Loaded Images";
+            this.label_Images.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Clear.Location = new System.Drawing.Point(1035, 129);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(99, 23);
+            this.button_Clear.TabIndex = 12;
+            this.button_Clear.Text = "Clear List";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button_Display
+            // 
+            this.button_Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Display.Location = new System.Drawing.Point(214, 129);
+            this.button_Display.Name = "button_Display";
+            this.button_Display.Size = new System.Drawing.Size(99, 23);
+            this.button_Display.TabIndex = 13;
+            this.button_Display.Text = "Display Image";
+            this.button_Display.UseVisualStyleBackColor = true;
+            this.button_Display.Click += new System.EventHandler(this.button_Display_Click);
+            // 
+            // button_Remove
+            // 
+            this.button_Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Remove.Location = new System.Drawing.Point(623, 129);
+            this.button_Remove.Name = "button_Remove";
+            this.button_Remove.Size = new System.Drawing.Size(99, 23);
+            this.button_Remove.TabIndex = 14;
+            this.button_Remove.Text = "Remove Image";
+            this.button_Remove.UseVisualStyleBackColor = true;
+            this.button_Remove.Click += new System.EventHandler(this.button_Remove_Click);
+            // 
+            // pB_Image
+            // 
+            this.pB_Image.Image = ((System.Drawing.Image)(resources.GetObject("pB_Image.Image")));
+            this.pB_Image.InitialImage = ((System.Drawing.Image)(resources.GetObject("pB_Image.InitialImage")));
+            this.pB_Image.Location = new System.Drawing.Point(214, 158);
+            this.pB_Image.Name = "pB_Image";
+            this.pB_Image.Size = new System.Drawing.Size(920, 729);
+            this.pB_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pB_Image.TabIndex = 15;
+            this.pB_Image.TabStop = false;
+            // 
+            // label_NoSelectImage
+            // 
+            this.label_NoSelectImage.AutoSize = true;
+            this.label_NoSelectImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NoSelectImage.ForeColor = System.Drawing.Color.Red;
+            this.label_NoSelectImage.Location = new System.Drawing.Point(344, 136);
+            this.label_NoSelectImage.Name = "label_NoSelectImage";
+            this.label_NoSelectImage.Size = new System.Drawing.Size(147, 13);
+            this.label_NoSelectImage.TabIndex = 16;
+            this.label_NoSelectImage.Text = "Error: Please Select an Image";
+            this.label_NoSelectImage.Visible = false;
+            // 
+            // YIM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1155, 891);
+            this.Controls.Add(this.label_NoSelectImage);
+            this.Controls.Add(this.pB_Image);
+            this.Controls.Add(this.button_Remove);
+            this.Controls.Add(this.button_Display);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.label_Images);
+            this.Controls.Add(this.lB_Images);
+            this.Controls.Add(this.cB_Skin);
+            this.Controls.Add(this.Button_Load);
+            this.Controls.Add(this.Label_HTTPFail);
+            this.Controls.Add(this.rb_LL);
+            this.Controls.Add(this.label_Skin);
+            this.Controls.Add(this.label_Character);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cB_Character);
+            this.Name = "YIM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lil Legend Image Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pB_Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cB_Character;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_Character;
+        private System.Windows.Forms.Label label_Skin;
+        private System.Windows.Forms.RadioButton rb_LL;
+        private System.Windows.Forms.Label Label_HTTPFail;
+        private System.Windows.Forms.Button Button_Load;
+        private System.Windows.Forms.CheckedListBox cB_Skin;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox lB_Images;
+        private System.Windows.Forms.Label label_Images;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Display;
+        private System.Windows.Forms.Button button_Remove;
+        private System.Windows.Forms.PictureBox pB_Image;
+        private System.Windows.Forms.Label label_NoSelectImage;
+    }
+}
+
---
+++ LilLegendViewer/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace LilLegendViewer
+{
+    public partial class YIM : Form
+    {
+        // Instantiate our HTTP Client
+        WebClient webClient = new WebClient();
+
+        // The URLs skimmed from the main targetted page, with their
+        // relevant names as keys
+        Dictionary<string, string> urls = new Dictionary<string, string>();
+
+        // Contains the naming scheme of all identified valid skins 
+        // Key values are the little legends
+        // Values are the skin name and the associated tier
+        // e.g. Furyhorn, Furyhorn_Victorious_Tier_1.png
+        Dictionary<string, List<string>> skinName = new Dictionary<string, List<string>>();
+
+
+        // Dictionary to store the temporarily loaded images
+        Dictionary<string, Image> imageDict = new Dictionary<string, Image>();
+
+        Boolean LL_Loaded = false;
+
+        public YIM()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        // Check the selected radiobuttons under "Category" and send that request
+        // to GetUrl. It will then make that information available in the 
+        // Dictionary skinName. 
+        private void Button_Load_Click(object sender, EventArgs e)
+        {
+            // Avoid loading everything if we have loaded it already
+            if (LL_Loaded)
+            {
+                return;
+            }
+
+            // This always gets changed
+            string url = "Empty";
+            Label_HTTPFail.Visible = false;
+
+            // Determine the current radiobutton 
+            if (rb_LL.Checked)
+            {
+                url = "https://leagueoflegends.fandom.com/wiki/Little_Legend_(Teamfight_Tactics)";
+                string results;
+                try
+                {
+                    // Get the information from the Little Legends site
+                    results = webClient.DownloadString(url); // Catch the output
+                }
+                catch (WebException exception)
+                {
+                    Label_HTTPFail.Visible = true;
+                    Console.WriteLine("\nException Caught!\nMessage :{0} ", exception.Message);
+                    return;
+                }
+                Console.WriteLine("GotHere");
+
+                // Read line by line, eliminate line if failed criteria, recombinate into
+                // final string for regex matching
+                var sr = new StringReader(results);
+                string line;
+                string newResults = "";
+
+                Console.WriteLine(results.ToString());
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains("href") && line.Contains("base egg") && line.Contains("variants"))
+                    {
+                        newResults += line;
+                        Console.WriteLine(line.ToString());
+                    }
+                }
+
+                // Run the regex to isolate the names of little legends
+                Regex rxNames = new Regex(@"/wiki/(.+?)_\(Teamfight_Tactics\)");
+                MatchCollection matches = rxNames.Matches(newResults);
+
+                string previous = "";
+                // Iterate through each match and extract the name
+                foreach (Match match in matches)
+                {
+                    string result = match.Value;
+                    string text = result.Replace("/wiki/", "").Replace("_(Teamfight_Tactics)", "");
+
+                    if (text.Contains("_(Little_Legend)"))
+                    {
+                        text = text.Replace("_(Little_Legend)", "");
+                    }
+
+                    if (!text.Equals(previous))
+                    {
+                        // Add to the dictionary of urls to download
+                        urls.Add(text, ("https://leagueoflegends.fandom.com" + result));
+
+                        // Add to names in the Character comboBox
+                        if (!cB_Character.Items.Contains(text))
+                        {
+                            cB_Character.Items.Add(text);
+                        }
+                        
+                    }
+                    previous = text;
+                }
+
+                // Regex for pulling skin names out of the little legends pages
+                Regex rxSkins = new Regex(@"/(\w+)_(\w+)_Tier_(.).png");
+
+                // Download the contents from each url and add it to the combo boxes
+                foreach (KeyValuePair<string, string> name_url in urls)
+                {
+                    // Grab the contents so we skim it for skins
+                    results = webClient.DownloadString(name_url.Value);
+
+                    matches = rxSkins.Matches(results);
+
+                    List<string> skins = new List<string>();
+
+                    foreach (Match match in matches)
+                    {
+                        string skinName = match.Value.Replace("/" + name_url.Key + "_", "");
+
+                        if (!skins.Contains(skinName))
+                        {
+                            // Eliminate incorrectly skimmed skins (skins not specifically for the little legend in question)
+                            if (match.Value.Contains(name_url.Key))
+                            {
+                                skins.Add(skinName);
+                            }
+                        }
+                    }
+                    skinName[name_url.Key] = skins;
+                }
+            }
+            label_Character.Visible = true;
+            cB_Character.Visible = true;
+            LL_Loaded = true;
+        }
+
+        private void cB_Character_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            cB_Skin.Items.Clear();
+
+            // Get the corresponding skin names for the selected Little Legend
+            string name = cB_Character.SelectedItem.ToString();
+            List<string> results = skinName[name];
+
+            foreach (string skin in results)
+            {
+                // Add to skin collection
+                char[] trimChars = { '_' };
+                string skinTrimmed = skin.Replace("Tier_", "").Replace(".png", "").Trim(trimChars);
+
+                cB_Skin.Items.Add(skinTrimmed);
+            }
+
+            // Now that we've add the skins, we need to check if any of them should be checked
+            // i.e. they exist in the listbox of images
+            //foreach (object item in lB_Images.Items){ }
+
+            label_Skin.Visible = true;
+            cB_Skin.Visible = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cB_Skin_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            int selected = cB_Skin.SelectedIndex;
+            string item =  cB_Skin.Items[selected].ToString();
+            string name = cB_Character.SelectedItem.ToString() + "_" + item;
+
+            // If the selected item is checked, we add it to the listBox
+            if (cB_Skin.CheckedItems.Contains(item))
+            {
+                // Ensure it's not already there (LAZY BUG FIX)
+                if (!lB_Images.Items.Contains(name))
+                {
+                    lB_Images.Items.Add(name);
+
+
+                    // Check if we don't already have the image downloaded
+                    if (imageDict.ContainsKey(name))
+                    {
+                        return;
+                    }
+
+                    // Now the fun stuff, we download the image and add it to the imagelist
+                    // Our current name is, for example:
+                    //  Featherknight_Pengu_1
+                    // We need it to be"
+                    //  ?file=Featherknight_Pengu_Tier_1.png
+
+                    // So we'll split first on the underscore
+                    string tier = item.Substring(item.Length - 1);
+                    string skin = item.Substring(0, item.Length -2);
+                    string[] pieces = { cB_Character.SelectedItem.ToString(), skin, tier };
+
+                    // Now we recombinate the pieces
+                    string fileName = pieces[0] + "_" + pieces[1] + "_Tier_" + pieces[2] + ".png";
+                    string url = urls[pieces[0]] + @"?file=" + fileName;
+
+                    // Okay so we messed up and literally need to download the file not 
+                    // from what we thought but instead a url like this:
+                    // https://static.wikia.nocookie.net/leagueoflegends/images/d/d6/Featherknight_Pengu_Tier_1.png/revision/latest/scale-to-width-down/1000?cb=20190612162426
+
+                    // To do that we need to redownload the html and locate the, say, above url
+                    string results = webClient.DownloadString(urls[pieces[0]]);
+
+                    Regex getImageURL = new Regex("\"{1}" + @"https://static.wikia.nocookie.net/leagueoflegends/images/\w{1}/\w{2}/" + fileName + @"/revision/latest\?cb=\d{1,}?" + "\"{1}");
+
+                    char[] toTrim = { '"' };
+                    string imageURL = getImageURL.Match(results).Value;
+                    imageURL = imageURL.Trim(toTrim);
+
+                    // Download the file and attach to imagelist
+                    Byte[] data = webClient.DownloadData(imageURL);
+
+                    Image image = (Bitmap)((new ImageConverter()).ConvertFrom(data));
+                    imageDict[name] = image;
+
+
+                }
+            }
+            // Else we remove it from the listBox
+            else
+            {
+                // Ensure it's actually there
+                if (lB_Images.Items.Contains(name))
+                {
+                    lB_Images.Items.Remove(name);
+                }
+            }
+        }
+
+        private void button_Display_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected
+            if (lB_Images.SelectedIndex.Equals(-1))
+            {
+                label_NoSelectImage.Visible = true;
+                return;
+            }
+
+            label_NoSelectImage.Visible = false;
+
+            // An item is selected, so grab the image from the imageDict
+            pB_Image.Image = imageDict[lB_Images.SelectedItem.ToString()];
+        }
+
+        private void button_Remove_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected
+            if (lB_Images.SelectedIndex.Equals(-1))
+            {
+                label_NoSelectImage.Visible = true;
+                return;
+            }
+
+            label_NoSelectImage.Visible = false;
+
+            // Remove the selected item from the collection
+            lB_Images.Items.Remove(lB_Images.SelectedItem);
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            lB_Images.Items.Clear();
+        }
+    }
+}
